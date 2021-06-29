94390b91bf8247f8dcaa27de481129b502e18c2d
SHREYAAS1000 <75013816+SHREYAAS1000@users.noreply.github.com>
2021-06-29 18:38:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+ function preload()
+ {}
+ function setup()
+ {
+     canvas=createCanvas(200,200);
+     canvas.center();
+     video=createCapture(VIDEO);
+     video.hide();
+     classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Yv6orROVi/model.json",modelLoaded);
+ }
+
+ function draw()
+ {
+     image(video,0,0,200,200);
+     classifier.classify(video, gotresult)
+ }
+
+function modelLoaded()
+{
+    console.log("Model is Loaded")
+}
+
+function gotresult(error,results){
+    if (error){
+        console.error(error);
+    }else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_accuracy_name").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
